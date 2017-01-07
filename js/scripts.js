9e894067034ca667f47bd7a3a68d4654750139b5
Szymon <szymon.grotha@gmail.com>
2017-01-07 16:56:33 +0100

    Letter check funcion - working

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 var newGameBtn = document.getElementById('js-newgame'),
-	jsMain = document.getElementById('main');
-	jsAlphabet = document.getElementById('alphabet')
+	jsMain = document.getElementById('main'),
+	jsAlphabet = document.getElementById('alphabet');
+	cars = ["SABB", "VOLVO", "BMW"];
+	fruits = ["BANANA", "ORANGE", "APPLE", "MANGO"];
+	animals = ['CAT', 'DOG', 'HORSE', 'DUCK']
 
 newGameBtn.addEventListener('click', newGameStart);
-
 
 function newGameStart() {
 	jsMain.style.display = 'none';
 	jsAlphabet.style.display = 'block';
 }
+
+$('.letter').on('click', function() {
+    var pickedLetter = ($(this).html());
+    console.log(pickedLetter)
+    return pickedLetter;
+});
+
